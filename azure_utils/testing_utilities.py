5c636352d935325e00981f8549b7e8ca0c6fad4d
Dan <dciborow@microsoft.com>
2020-02-15 11:00:59 -0500

    Updates with tests

---
+++ azure_utils/testing_utilities.py
@@ -0,0 +1,117 @@
+"""
+AI-Utilities - testing_utilities.py
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+"""
+import json
+import os
+import re
+import sys
+
+import nbformat
+from nbconvert import MarkdownExporter, RSTExporter
+import papermill as pm
+from junit_xml import TestCase, TestSuite
+
+NOTEBOOK_OUTPUT_EXT = ".output_ipynb"
+
+
+def run_notebook(input_notebook, add_nunit_attachment, parameters=None, kernel_name="ai-architecture-template",
+                 root="."):
+    """
+    Used to run a notebook in the correct directory.
+
+    Parameters
+    ----------
+    :param input_notebook: Name of Notebook to Test
+    :param add_nunit_attachment:
+    :param parameters:
+    :param kernel_name: Jupyter Kernal
+    :param root:
+    """
+
+    output_notebook = input_notebook.replace(".ipynb", NOTEBOOK_OUTPUT_EXT)
+    try:
+        results = pm.execute_notebook(
+            os.path.join(root, input_notebook),
+            os.path.join(root, output_notebook),
+            parameters=parameters,
+            kernel_name=kernel_name
+        )
+
+        for cell in results.cells:
+            if cell.cell_type is "code":
+                assert not cell.metadata.papermill.exception, "Error in Python Notebook"
+    finally:
+        with open(os.path.join(root, output_notebook)) as json_file:
+            data = json.load(json_file)
+            jupyter_output = nbformat.reads(json.dumps(data), as_version=nbformat.NO_CONVERT)
+
+        export_md(jupyter_output, output_notebook, add_nunit_attachment, file_ext=".txt", root=root)
+        export_rst(jupyter_output, output_notebook, add_nunit_attachment, root=root)
+
+        regex = r'Deployed (.*) with name (.*). Took (.*) seconds.'
+
+        with open(os.path.join(root, output_notebook), 'r') as file:
+            data = file.read()
+
+            test_cases = []
+            for group in re.findall(regex, data):
+                test_cases.append(
+                    TestCase(name=group[0] + " creation", classname=input_notebook, elapsed_sec=float(group[2]),
+                             status="Success"))
+
+            ts = TestSuite("my test suite", test_cases)
+
+            with open('test-timing-output.xml', 'w') as f:
+                TestSuite.to_file(f, [ts], prettyprint=False)
+
+
+def export_notebook(exporter, jupyter_output, output_notebook, add_nunit_attachment, file_ext, root='.'):
+    """
+    Export Jupyter Output to File
+
+    :param exporter:
+    :param jupyter_output:
+    :param output_notebook:
+    :param add_nunit_attachment:
+    :param file_ext:
+    :param root:
+    """
+    (body, resources) = exporter.from_notebook_node(jupyter_output)
+    with open(os.path.join(root, output_notebook.replace(NOTEBOOK_OUTPUT_EXT, file_ext)), "w") as text_file:
+        sys.stderr.write(body)
+        text_file.write(body)
+
+    if add_nunit_attachment is not None:
+        path = os.path.join(root, output_notebook.replace(NOTEBOOK_OUTPUT_EXT, file_ext))
+        add_nunit_attachment(path, output_notebook)
+
+
+def export_md(jupyter_output, output_notebook, add_nunit_attachment, file_ext='.md', root="."):
+    """
+    Export Jupyter Output to Markdown File
+
+    :param jupyter_output:
+    :param output_notebook:
+    :param add_nunit_attachment:
+    :param file_ext:
+    :param root:
+    """
+    markdown_exporter = MarkdownExporter()
+    export_notebook(markdown_exporter, jupyter_output, output_notebook, add_nunit_attachment, file_ext, root=root)
+
+
+def export_rst(jupyter_output, output_notebook, add_nunit_attachment, file_ext='.rst', root="."):
+    """
+    Export Jupyter Output to RST File
+
+    :param jupyter_output:
+    :param output_notebook:
+    :param add_nunit_attachment:
+    :param file_ext:
+    :param root:
+    """
+    rst_exporter = RSTExporter()
+    export_notebook(rst_exporter, jupyter_output, output_notebook, add_nunit_attachment, file_ext, root=root)
--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
+"""
+AI-Utilities - setup.py
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+"""
 from setuptools import setup, find_packages
 
 setup(
-    name="Microsoft-AI-Azure-Utilities",	     # This is the name of your PyPI-package.
-    version="0.2.0",		     # Update the version number for new releases
-    description="Utility Functions for AI Solutions",
+    name="Microsoft-AI-Azure-Utility-Samples",	        # This is the name of your PyPI-package.
+    version="0.2.2",		                            # Update the version number for new releases
+    description="Utility Samples for AI Solutions",
     author="Daniel Ciborowski & Daniel Grecoe",
     author_email="dciborow@microsoft.com",
     url="https://github.com/microsoft/AI-Utilities",
     license="MIT",
     packages=find_packages(),
-    install_requires=["azure-cli", 'azureml-core', 'python-dotenv']
+    install_requires=["azure-cli", 'azureml-core', 'python-dotenv', 'nbformat', 'papermill', 'nbconvert', 'junit_xml']
 )
