021d984ffca2ec5198c37d85e8e95d704e8575c8
Daniel Ciborowski <dciborow@microsoft.com>
2020-02-25 09:03:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Microsoft-AI-Azure-Utility-Samples",  # This is the name of your PyPI-package.
-    version="0.2.9",  # Update the version number for new releases
+    version="0.2.10",  # Update the version number for new releases
     description="Utility Samples for AI Solutions",
     author="Daniel Ciborowski & Daniel Grecoe",
     author_email="dciborow@microsoft.com",
