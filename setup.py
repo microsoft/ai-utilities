adc06a386584f22a4da8e03d6b059aead54d3c4c
Dan <dciborow@microsoft.com>
2020-03-18 11:38:16 -0400

    adding fpga

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Licensed under the MIT License.
 from setuptools import find_packages, setup
 
 # This is the name of your PyPI-package.
-setup(name="Microsoft-AI-Azure-Utility-Samples", version="0.3.84", description="Utility Samples for AI Solutions",
+setup(name="Microsoft-AI-Azure-Utility-Samples", version="0.3.85", description="Utility Samples for AI Solutions",
       author="Daniel Ciborowski & Daniel Grecoe", author_email="dciborow@microsoft.com",
       url="https://github.com/microsoft/AI-Utilities", license="MIT", packages=find_packages(),
       install_requires=['resnet'])
